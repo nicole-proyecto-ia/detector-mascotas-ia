46dc4980353c9f4a80a9135ee2aa0f6d4d3cffcb
nicole-proyecto-ia <mhola4634@gmail.com>
2026-07-02 23:12:45 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+const URL = "https://teachablemachine.withgoogle.com/models/6Vgctrh9W/";
+
+let model;
+let webcam;
+let labelContainer;
+let maxPredictions;
+
+async function iniciarIA() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    webcam = new tmImage.Webcam(300, 300, true);
+    await webcam.setup();
+    await webcam.play();
+
+    window.requestAnimationFrame(loop);
+
+    document.getElementById("webcam-container").innerHTML = "";
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+    labelContainer = document.getElementById("label-container");
+    labelContainer.innerHTML = "";
+
+    for (let i = 0; i < maxPredictions; i++) {
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update();
+    await predecir();
+    window.requestAnimationFrame(loop);
+}
+
+async function predecir() {
+    const prediction = await model.predict(webcam.canvas);
+
+    for (let i = 0; i < maxPredictions; i++) {
+        const clase = prediction[i].className;
+        const porcentaje = (prediction[i].probability * 100).toFixed(2);
+
+        labelContainer.childNodes[i].innerHTML = clase + ": " + porcentaje + "%";
+    }
+}
